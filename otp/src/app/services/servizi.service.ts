68b27fae9329393a47d6bd738793f81462824b04
alessiofor9 <fornaro47@gmail.com>
2021-03-17 14:42:05 +0100

    servizi modificati

--- otp/src/app/services/servizi.service.ts
+++ otp/src/app/services/servizi.service.ts
@@ -8,24 +8,47 @@ import { IdPc, Utente } from '../classi/Classe';
 export class ServiziService {
 
   constructor(private http : HttpClient) { }
+
+ //MODIFICATO
   creautente(id: number,mail: string)
   {
-    return this.http.post(`http://localhost:8080/crea/${id}/${mail}`,null);
+    return this.http.post(`http://localhost:8080/totp/user/${id}/${mail}`,null);
   }
+
+  //MODIFICATO
   controlloEsistente(id : number)
   {
-    return this.http.get<boolean>(`http://localhost:8080/esistente/${id}`);
+    return this.http.get<boolean>(`http://localhost:8080/totp/user/${id}`);
   }
 
-  login(u : Utente)
+  //MODIFICATO
+  login(id : number, otp : string)
   {
-    return this.http.post<boolean>(`http://localhost:8080/accesso`,u);
+    return this.http.get(`http://localhost:8080/totp/user/${id}/otp/${otp}`);
   }
 
+  // MODIFICATO
   recuperaQr(username : string)
   {
-    return this.http.get<Utente>(`http://localhost:8080/recupera/${username}`);
+    return this.http.get<Utente>(`http://localhost:8080/totp/user/${username}/qrcode`);
   }
+
+  // MODIFICATO
+  modificaUtente(id : number , mail :string)
+  {
+    return this.http.put(`http://localhost:8080/totp/user/${id}/${mail}`, null);
+  }
+
+  //MODIFICATO
+  eliminaUtente(id:number)
+  {
+    return this.http.delete(`http://localhost:8080/totp/user/${id}`);
+  }
+
+
+
+
+  
 
   creaidpc(a : IdPc)
   {
